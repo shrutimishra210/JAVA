be145b443680d7ee158c8b81c5456a82686e8ce8
shrutimishra210 <shrutimishra2105@gmail.com>
2026-07-13 14:57:07 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,7 @@
+class intro
+{
+   public static void main(String[] args)
+  {
+   System.out.println("hello\nI am Shruti Mishra\nfrom BCA 3C in MarwadiUniversity\nbasically i'm from Prayagraj,UP\ndone my schooling from K.V.S\nI like coding,reading books and playing outdoor games\nthis is my contact and Email details\nI want to build my career as a web developer.");	
+  }
+}
