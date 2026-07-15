dc5f2685c9bb012ffb08413985b47159436f7e7d
shrutimishra210 <shrutimishra2105@gmail.com>
2026-07-15 14:36:05 +0530

    Add files via upload

---
+++ forname.java
@@ -0,0 +1,26 @@
+//6.4 wrte the program to get the name from the user and print 10 times using forloop.
+
+import java.util.Scanner;
+public class forname
+    {
+	    public static void main(String args[])
+		
+		{
+		   Scanner scn= new Scanner(System.in);
+		   
+		   System.out.println("Enter the Name:");
+		   String Name=scn.nextLine();
+		   
+		    int i;
+		   for (i=1;i<=10;i++)
+		   
+		   System.out.println("Entered Name: " + Name);
+		   
+		    scn.close();
+		   
+		   
+	    }
+
+			
+			
+    }
