e8fe32d2c796ff3f0c2ee4e755d79fe99ae7fbd1
shrutimishra210 <shrutimishra2105@gmail.com>
2026-08-07 15:48:59 +0530

    Create u1p6_12.java

---
+++ UNIT1/u1p6_12.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+	
+	public class u1p6_12
+	{
+		public static void main(String args[])
+		{
+			Scanner scn = new Scanner(System.in);
+			
+			
+			
+            System.out.println("Enter the limit:");
+			int n=scn.nextInt();
+			scn.close();
+        
+        for (int i = n; i >= 1; i--)
+		{
+             
+            for (int j = i; j >= 1;j--)
+				{
+                System.out.print("*");
+                }
+            System.out.println("");
+        }
+    }
+}
