aa718446fd75c35693a71597ffcd8b91df5941c2
shrutimishra210 <shrutimishra2105@gmail.com>
2026-09-09 14:41:56 +0530

    Create u2p8_1.java

---
+++ UNIT2/u2p8_1.java
@@ -0,0 +1,10 @@
+public class u2p8_1{
+    public static void main(String args[]){
+        final int roll_no=101;
+        final double marks=100.0;
+        System.out.println("Roll no :"+roll_no);
+        //roll_no=102;
+        //System.out.println("Roll no :"+roll_no);
+        // error: cannot assign a value to final variable roll_no
+    }
+}
