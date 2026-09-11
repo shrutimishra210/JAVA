c059edd3f1f1014c7aa55d487eff52d2515adeac
shrutimishra210 <shrutimishra2105@gmail.com>
2026-09-11 20:30:34 +0530

    Create u2p11.java

---
+++ UNIT2/u2p11.java
@@ -0,0 +1,23 @@
+//Write a java program to perform overriding of abstract class.
+abstract class Animal
+{
+    abstract void sound();
+}
+
+
+class Dog extends Animal
+{
+    void sound()
+    {
+        System.out.println("Dog barks");
+    }
+}
+
+public class u2p11
+{
+    public static void main(String[] args)
+    {
+        Animal a = new Dog();
+        a.sound();
+    }
+}
