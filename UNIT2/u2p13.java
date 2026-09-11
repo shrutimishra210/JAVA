b0f3df37df6f8e905625e1372172793314a7a9fb
shrutimishra210 <shrutimishra2105@gmail.com>
2026-09-11 20:33:13 +0530

    Create u2p13.java

---
+++ UNIT2/u2p13.java
@@ -0,0 +1,9 @@
+//Write a java program of package.
+package mypack;
+public class u2p13
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is package");
+    }
+}
