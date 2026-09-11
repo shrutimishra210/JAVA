3d214269b7c5fb726315b8b188cfdc9856b34598
shrutimishra210 <shrutimishra2105@gmail.com>
2026-09-11 20:29:46 +0530

    Create u2p10.java

---
+++ UNIT2/u2p10.java
@@ -0,0 +1,18 @@
+//Write a java program to demonstrate encapsulation.
+public class u2_p10{
+	private String name;
+	public String getName()
+	{
+	  return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public static void main(String[] args){
+		U2_P10 emp = new U2_P10();
+		emp.setName("Diya");
+		System.out.println("Employee Name :" + emp.getName());
+	}
+		
+}
